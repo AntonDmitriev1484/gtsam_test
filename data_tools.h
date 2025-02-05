95571dcb01dafef41a29bf7ba42e8e5dbe5b485c
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2025-02-05 14:38:00 -0600

    Removed EuRoC code. EuRoC code still on IMU-preintegration-viz

--- data_tools.h
+++ data_tools.h
@@ -16,10 +16,6 @@ using namespace gtsam;
 
 using json = nlohmann::json;
 
-bool parse_EuRoC_gt_line(ifstream& gt_file, Point3& position, Rot3& rotation,
-	Vector3& velocity, Vector3& gyro_bias, Vector3& accel_bias);
-bool parse_EuRoC_imu_line(ifstream& imu_file, Vector3& V_angular, Vector3& A_axial);
-
 // Cappella parsing code
 struct tracking_info {
 
