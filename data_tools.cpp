c366ad2b9decb6b5868f006f0fe6a2bf41508c95
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2025-02-05 18:33:45 -0600

    Fixed extra nesting in dump_reconst

--- data_tools.cpp
+++ data_tools.cpp
@@ -164,10 +164,10 @@ void dump_reconstructed_trajectories(map<string, tracking_info> info, string fil
 					{"type", "gt_reconstruct"},
 					{"user", user},
 					{"pose", {
-						{ {m(0,0),m(0,1), m(0,2), m(0,3)}},
-						{ {m(1,0),m(1,1), m(1,2), m(1,3)}},
-						{ {m(2,0),m(2,1), m(2,2), m(2,3)}},
-						{ {m(3,0),m(3,1), m(3,2), m(3,3)}},
+						{ m(0,0),m(0,1), m(0,2), m(0,3)},
+						{ m(1,0),m(1,1), m(1,2), m(1,3)},
+						{ m(2,0),m(2,1), m(2,2), m(2,3)},
+						{ m(3,0),m(3,1), m(3,2), m(3,3)},
 					}
 			}
 			};
