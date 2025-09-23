e6fa284cd17552f36cdc766630027b1fd2dac2cc
AntonDmitriev1484 <71272032+AntonDmitriev1484@users.noreply.github.com>
2025-09-22 19:26:44 -0500

    Cleaned CMakeLists

--- gtsam_test.cpp
+++ gtsam_test.cpp
@@ -1,6 +1,5 @@
 ﻿#include "gtsam_test.h"
 #include "data_tools.h"
-#include "utils.h"
 #include "cmath"
 #include "tracker.h"
 #include <regex>
@@ -120,27 +119,24 @@ int main(int argc, char* argv[]) {
 	imuBias::ConstantBias prior_imu_bias(Vector3(-0.03, -0.05, 0.23), Vector3(-0.0048, -0.00445, -0.0015)); 
 	// First accelerometer bias, then gyro bias.
 	
-	// Pose3 T_body_to_imu;
-	// get_pose_from_HTM(transforms["T_body_to_imu"], T_body_to_imu);
-	// Matrix33 rot_body_to_imu; // Alternative frame that looks "more reasonable" but is incorrect
-	// rot_body_to_imu <<  0,1,0,
-	// 					0,0,1,
-	// 					1,0,0;
+	Pose3 T_imu_to_body;
+	get_pose_from_HTM(transforms["T_imu_to_body"], T_imu_to_body);
 
-	// Judging from this transform from earlier
-	Matrix33 transform;
-	transform << 1, 0, 0,
-				0, 0, 1,
-				0, -1, 0;
-	// we're dealing with a y-forward, x-right body frame.
-	Pose3 T_imu_to_body(Rot3(transform), Vector3::Zero());
+	// // Judging from this transform from earlier
+	// Matrix33 transform;
+	// transform << 1, 0, 0,
+	// 			0, 0, 1,
+	// 			0, -1, 0;
+	// // we're dealing with a y-forward, x-right body frame.
+	// Pose3 T_imu_to_body(Rot3(transform), Vector3::Zero());
+	// Pose3 T_body_to_imu = T_imu_to_body.inverse();
+
 	Pose3 T_body_to_imu = T_imu_to_body.inverse();
-	// imu_preintegration_params->setBodyPSensor(T_imu_to_body.inverse());
-	// imu_preintegration_params->setBodyPSensor(T_body_to_imu);
 
 	//https://gtsam.org/doxygen/4.0.0/a03531.html
 	// "Pose of the sensor in the body frame" - this should be T IMU to body.
-	imu_preintegration_params->setBodyPSensor(T_imu_to_body);
+	// but T_body_to_imu seems to give a more reasonable looking (but still incorrect) result
+	imu_preintegration_params->setBodyPSensor(T_body_to_imu);
 
 
 
@@ -154,6 +150,7 @@ int main(int argc, char* argv[]) {
 	const int smoother_lag = 1;
 	const bool use_smoother = false;
 	const bool use_filter = false;
+
 
 	Tracker t(
 		id, T_body_to_imu, T_body_to_decawave, 
--- tracker.h
+++ tracker.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "gtsam_test.h"
-#include "nlohmann/json.hpp"
 #include "data_tools.h"
-#include "utils.h"
 #include "cmath"
 #include <regex>
 
